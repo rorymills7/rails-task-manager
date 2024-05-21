7401007084bd11cc89cfcc57ec256fab0b587c62
Rory Mills <rorymills7@gmail.com>
2024-05-21 14:39:04 +0100

    task complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :task_new
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create", as: :task_create
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update", as: :task_update
+  delete "tasks/:id", to: "tasks#destroy", as: :task_destroy
 end
